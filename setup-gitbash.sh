7ab9f05746aad99819ee99a2b2d9caa299038a53
Gábor Sinka <hessor@gmail.com>
2021-04-03 17:25:03 +0200

    Git-bash setup script (for vimfiles)

---
+++ setup-gitbash.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf "$PWD/bashrc" "$HOME/.bashrc"
+ln -sf "$PWD/shell.d" "$HOME/.shell.d"
+ln -sf "$PWD/vim/vimrc" "$HOME/.vimrc"
+ln -sf "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$PWD/inputrc" "$HOME/.inputrc"
+ln -sf "$HOME/.vim" "$HOME/vimfiles"
