bdb2f895f3fea9534dd71b0ae17d38eea1cc59b2
Gábor Sinka <hessor@gmail.com>
2021-07-29 21:30:54 +0200

    Updated php isntall script to support php8.0

--- install/install-php.sh
+++ install/install-php.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# old installation - limited to php 7.4
+#sudo apt update
+#sudo apt install php php-xml
+#sudo apt install composer
+
 sudo apt update
-sudo apt install php php-xml
-sudo apt install composer
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt update
+sudo apt install php7.4
+
+# to install php 8
+#sudo apt install php8.0
