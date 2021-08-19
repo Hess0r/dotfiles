1ff548b28cba52fcd3d88946e158dfffdb96de66
Sinka Gábor <gabor.sinka@mmsone.com>
2021-08-19 11:05:55 +0200

    New aliases for git commands

--- shell.d/aliases.sh
+++ shell.d/aliases.sh
@@ -1,3 +1,4 @@
+# standard ubuntu coloring
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
@@ -6,6 +7,7 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
+# standard ubuntu ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
@@ -29,4 +31,20 @@ alias dcpull='docker-compose pull'
 alias dcstart='docker-compose start'
 alias dck='docker-compose kill'
 
+# alias for Laravel Sail binary
 alias sail='./vendor/bin/sail'
+
+# directory shortcuts
+alias dotfiles='cd ~/dotfiles'
+
+# git aliases
+alias ga='git add'
+alias gaa='git add --all'
+alias gb='git branch'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias glog="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset%s%Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+alias glogs="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen(%cr) %C(bold blue)<%an>%Creset' --stat"
+alias glogds="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
+alias gloga="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen(%cr) %C(bold blue)<%an>%Creset' --all"
+
