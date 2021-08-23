3ed8b99c90246b6c623f2fe17813eabfc0268e36
Sinka Gábor <gabor.sinka@mmsone.com>
2021-08-23 15:58:31 +0200

    Php 8 install script

---
+++ install/install-php8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt install php8.0
+sudo apt install php8.0-fileinfo \
+  php8.0-mbstring \
+  php8.0-bcmath \
+  php8.0-cli \
+  php8.0-common \
+  php8.0-dom \
+  php8.0-curl \
+  php8.0-gd \
+  php8.0-intl \
+  php8.0-mbstring \
+  php8.0-opcache \
+  php8.0-xml \
