16567a6b810f812435d040eebc404a4298815314
Gábor Sinka <hessor@gmail.com>
2021-11-04 19:24:25 +0100

    Gvimrc completely separated

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,1 @@
-# Copy-Item -Path ./vim/vimrc_windows -Destination $home/_vimrc
-# Copy-Item -Path ./vim/gvimrc -Destination $home/_gvimrc
 New-Item -Path $home/_gvimrc -ItemType SymbolicLink -Value ./vim/gvimrc 
