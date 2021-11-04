aa520adbe05badbf527a962227e200ff0a46bd71
Hess0r <hessor@gmail.com>
2021-11-04 20:24:56 +0100

    Gvim and windows vimrc refactor, setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,2 @@
-New-Item -Path $home/_gvimrc -ItemType SymbolicLink -Value ./vim/gvimrc 
+New-Item -Path $home/_vimrc -ItemType SymbolicLink -Value ./vim/vimrc_windows -Force
+New-Item -Path $home/_gvimrc -ItemType SymbolicLink -Value ./vim/gvimrc  -Force
