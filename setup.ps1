056ac623eb6fc3070f4d632cb6c17f0cc3ea7d7d
Sinka Gábor <gabor.sinka@mmsone.com>
2021-11-04 16:55:03 +0100

    Gvimrc setup cont.
    Probably should separate gvimrc vimrc and vimrc_windows completely
    vim in powershell is pretty crappy anyways...

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,3 @@
-Copy-Item -Path ./vim/vimrc_windows -Destination $home/_vimrc
-Copy-Item -Path ./vim/gvimrc -Destination $home/_gvimrc
+# Copy-Item -Path ./vim/vimrc_windows -Destination $home/_vimrc
+# Copy-Item -Path ./vim/gvimrc -Destination $home/_gvimrc
+New-Item -Path $home/_gvimrc -ItemType SymbolicLink -Value ./vim/gvimrc 
