f06b283afed5fa68ef6b3bc681f782577978c099
Sinka Gábor <gabor.sinka@mmsone.com>
2021-11-04 13:36:25 +0100

    Removed git-bash setup script

--- setup-gitbash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -sf "$PWD/bashrc" "$HOME/.bashrc"
-ln -sf "$PWD/shell.d" "$HOME/.shell.d"
-ln -sf "$PWD/vim/vimrc" "$HOME/.vimrc"
-ln -sf "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$PWD/inputrc" "$HOME/.inputrc"
-ln -sf "$HOME/.vim" "$HOME/vimfiles"
