19bb788e641cc64aec987eeef4d150509944386d
Sinka Gabor <gsinka@pop-os.localdomain>
2021-12-11 12:10:54 +0100

    Changes to install

--- install/essentials.sh
+++ install/essentials.sh
@@ -10,16 +10,13 @@ sudo apt install \
   jq \
   ripgrep \
   tree \
-  software-properties-common \
-  neovim
+  tmux \
+  software-properties-common
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo add-apt-repository ppa:neovim-ppa/stable
 
 sudo apt update
 sudo apt install \
-  gh \
   neovim
 
 mkdir -p ~/.local/bin
--- install/setup-keychron.sh
+++ install/setup-keychron.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Find a way to use the permanent solution:
+# /etc/modprobe.d/hid_apple.conf
+# options hid_apple fnmode=0
+
 echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
