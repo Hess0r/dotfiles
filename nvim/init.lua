2b7be8ea01b36c2c9f6b5c3ea52cc0a8dd38da45
Gábor Sinka <hessor@gmail.com>
2022-01-11 19:45:49 +0100

    Remove lua lsp for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -239,27 +239,28 @@ nvim_lsp.jsonls.setup(config({
   }
 }))
 
-nvim_lsp.sumneko_lua.setup(config({
-  cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-        path = vim.split(package.path, ";"),
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-        },
-        preloadFileSize = 200,
-      },
-    },
-  },
-}))
+-- nvim_lsp.sumneko_lua.setup(config({
+--   cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         version = "LuaJIT",
+--         path = vim.split(package.path, ";"),
+--       },
+--       diagnostics = {
+--         globals = { "vim" },
+--       },
+--       workspace = {
+--         library = {
+--           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+--           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+--         },
+--         preloadFileSize = 200,
+--         checkThirdParty = false,
+--       },
+--     },
+--   },
+-- }))
 
 local pid = vim.fn.getpid()
 nvim_lsp.omnisharp.setup(config({
