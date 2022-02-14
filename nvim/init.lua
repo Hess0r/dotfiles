14fc2fd3a90c72c9f283a594e8c350837ba7cfd1
Gábor Sinka <hessor@gmail.com>
2022-02-14 16:32:50 +0100

    Tmux settings, nvim clipboard copy

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,9 +27,12 @@ vim.opt.wildignore = vim.o.wildignore .. '*/vendor/*'
 vim.opt.wildignore = vim.o.wildignore .. '*/node_modules/*'
 vim.opt.wildmenu = true
 vim.opt.list = true
-vim.opt.listchars = 'tab:» ,eol:↲'
+-- this makes go really noisy
+-- vim.opt.listchars = 'tab:» ,eol:↲'
+vim.opt.listchars = 'tab:  ,eol:↲'
 vim.opt.completeopt = 'menu,menuone,noselect'
 vim.opt.splitright = true
+vim.opt.clipboard = "unnamedplus"
 
 -- PLUGINS
 local vim_plug_path = vim.fn.stdpath'data' .. '/site/autoload/plug.vim'
@@ -106,6 +109,9 @@ vim.api.nvim_set_keymap('n', '<leader>gb', [[<cmd>lua require('telescope.builtin
 
 vim.api.nvim_set_keymap('n', '<leader>n', [[<cmd>Lexplore %:p:h<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>m', [[<cmd>Lexplore<CR>]], opts)
+
+vim.api.nvim_set_keymap('n', '<leader>y', [["+y]], opts)
+vim.api.nvim_set_keymap('n', '<leader>p', [["+p]], opts)
 
 -- TREESITTER
 local nvim_treesitter = require'nvim-treesitter.configs'
@@ -190,7 +196,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<space>ca', '<cmd>lua require("telescope.builtin").lsp_code_actions()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', opts)
   buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
