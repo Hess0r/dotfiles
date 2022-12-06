9f79f8a7554e943af8aebebf8d02d862d3a5af72
Gabor Sinka <hessor@gmail.com>
2022-12-06 21:29:26 +0100

    Try to actualize this a bit

--- setup.sh
+++ setup.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-ln -sf "$PWD/bashrc" "$HOME/.bashrc"
+ln -sf "$PWD/bash/bashrc" "$HOME/.bashrc"
+ln -sf "$PWD/bash/inputrc" "$HOME/.inputrc"
 
-# TODO figure out a way to remove this
-ln -sf "$PWD/shell.d" "$HOME/.shell.d"
-
-ln -sf "$PWD/vim/vimrc" "$HOME/.vimrc"
 ln -sf "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
-ln -sf "$PWD/inputrc" "$HOME/.inputrc"
 [ -d "$PWD/secrets" ] && ln -sf "$PWD/secrets" "$HOME/.secrets"
 
 # mkdir -p "$HOME/.vim"
 
-#mkdir -p "$HOME/.config/nvim"
-# ln -sf "$PWD/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-
+rm -rf "$HOME/.config/nvim"
 ln -sf "$PWD/nvim" "$HOME/.config"
 
 mkdir -p "$HOME/.config/starship"
