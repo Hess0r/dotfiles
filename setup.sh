ff1c35997a73da7a9f97cdc92a0dce0ef45461e3
Sinka Gabor <gabor.sinka@mmsone.com>
2022-12-08 14:28:48 +0100

    tmux, starship

--- setup.sh
+++ setup.sh
@@ -5,18 +5,10 @@ ln -sf "$PWD/bash/inputrc" "$HOME/.inputrc"
 
 ln -sf "$PWD/shell.d" "$HOME/"
 
-# ln -sf "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
-# [ -d "$PWD/secrets" ] && ln -sf "$PWD/secrets" "$HOME/.secrets"
-
-# mkdir -p "$HOME/.vim"
+ln -sf "$PWD/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 rm -rf "$HOME/.config/nvim"
 ln -sf "$PWD/nvim" "$HOME/.config"
 
-# mkdir -p "$HOME/.config/starship"
-# ln -sf "$PWD/starship/config.toml" "$HOME/.config/starship/config.toml"
-
-# if type code &>/dev/null; then
-#   cp "$PWD/vscode/keybindings.json" "$HOME/.config/Code/User/keybindings.json"
-#   cp "$PWD/vscode/settings.json" "$HOME/.config/Code/User/settings.json"
-# fi
+mkdir -p "$HOME/.config/starship"
+ln -sf "$PWD/starship/config.toml" "$HOME/.config/starship"
