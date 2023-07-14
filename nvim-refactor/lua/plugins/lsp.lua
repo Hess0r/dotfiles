70bcf81dd7ba0fd391194a086fdd441aeae6867b
Gabor Sinka <hessor@gmail.com>
2023-07-14 19:40:33 +0200

    pin fidged version, fix zshrc editor settings

--- nvim-refactor/lua/plugins/lsp.lua
+++ nvim-refactor/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
       'b0o/schemastore.nvim',
       'jose-elias-alvarez/null-ls.nvim',
       'jayp0521/mason-null-ls.nvim',
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', opts = {}, tag = 'legacy' },
       {
         'folke/neodev.nvim',
         config = function()
