01b7005e8f596010b339f44b36bfa77a193de660
Gabor Sinka <gabor.sinka@attrecto.com>
2024-01-09 15:50:20 +0100

    go testing, cmp and tabs

--- nvim/lua/gsinka/plugins/vim-test.lua
+++ nvim/lua/gsinka/plugins/vim-test.lua
@@ -2,6 +2,8 @@ vim.cmd([[
   let test#php#phpunit#executable = 'vendor/bin/phpunit'
   let test#php#phpunit#options = '--colors=always --testdox'
 
+  let test#go#gotest#options = '-v'
+
   function! FloatermStrategy(cmd)
     execute 'silent FloatermKill test'
     execute 'FloatermNew --autoclose=2 --name=test ' . a:cmd . ' | bat --paging=always'
